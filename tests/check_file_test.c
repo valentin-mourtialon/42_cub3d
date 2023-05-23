3c679960473dce4baede46d41f96ff895bc06bf9
valentin-mourtialon <valentinmourtialon@protonmail.com>
2023-05-23 13:36:10 +0200

    V: Add tests folder

---
+++ tests/check_file_test.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_file_test.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 13:14:59 by vmourtia          #+#    #+#             */
+/*   Updated: 2023/05/23 13:35:32 by vmourtia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cub3d.h>
+
+/* Testing all the functions of the file "check_file.c" */ 
+
+typedef struct s_test {
+	char	*input;
+	char	*expected;
+}	t_test;
+
+void	check_file_test(void)
+{
+	t_test	test1;
+	
+}
+
+int	main(void) {
+	check_file_test();
+	return (0);
+}
