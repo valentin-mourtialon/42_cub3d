f8f711e77788869a530dfd3e1acd5ac34af929d8
valentin-mourtialon <valentinmourtialon@protonmail.com>
2023-05-23 12:43:35 +0200

    V: clear debug printf

--- sources/parser/check_file.c
+++ sources/parser/check_file.c
@@ -6,7 +6,7 @@
 /*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 11:50:41 by sel-maar          #+#    #+#             */
-/*   Updated: 2023/05/23 12:28:56 by vmourtia         ###   ########.fr       */
+/*   Updated: 2023/05/23 12:42:37 by vmourtia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ t_bool	check_extension(char *file, char *extension)
 	file_length = ft_strlen(file);
 	extension_length = ft_strlen(extension);
 	if (ft_strcmp(&file[file_length - extension_length], extension))
-		return (printf("check_extension(%s, %s) = false\n", file, extension), false);
-	return (printf("check_extension(%s, %s) = true\n", file, extension), true);
+		return (false);
+	return (true);
 }
