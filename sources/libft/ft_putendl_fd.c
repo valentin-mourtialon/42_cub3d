80f1c2777b8d8491f3b056d00981c81bcecfef48
valentin-mourtialon <valentinmourtialon@protonmail.com>
2023-05-25 14:36:55 +0200

    V: Now compiling. Solve the previous merge conflict.

--- sources/libft/ft_putendl_fd.c
+++ sources/libft/ft_putendl_fd.c
@@ -6,7 +6,7 @@
 /*   By: vmourtia <vmourtia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 11:50:41 by sel-maar          #+#    #+#             */
-/*   Updated: 2023/05/25 14:30:44 by vmourtia         ###   ########.fr       */
+/*   Updated: 2023/05/25 14:35:02 by vmourtia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,11 @@
 
 void	ft_putendl_fd(char *str, int fd)
 {
-	int	file_length;
-	int	extension_length;
-	
-	file_length = ft_strlen(file);
-	extension_length = ft_strlen(extension);
-	if (ft_strcmp(&file[file_length - extension_length], extension))
-		return (false);
-	return (true);
+	if (str == NULL)
+	{
+		write(fd, "\n", 1);
+		return ;
+	}
+	write(fd, str, ft_strlen(str));
+	write(fd, "\n", 1);
 }
