03d7aa3b70a7364814990b381c230afe65f8fe8c
valentin-mourtialon <88686764+valentin-mourtialon@users.noreply.github.com>
2023-06-29 10:13:26 +0200

    Update listener.c

--- sources/movement/listener.c
+++ sources/movement/listener.c
@@ -50,35 +50,6 @@ static int	key_release_handler(int key, t_data *data)
 	return (0);
 }
 
-static void	wrap_mouse_position(t_data *data, int x, int y)
-{
-	if (x > data->win_width - DIST_EDGE_MOUSE_WRAP)
-	{
-		x = DIST_EDGE_MOUSE_WRAP;
-		mlx_mouse_move(data->mlx, data->win, x, y);
-	}
-	if (x < DIST_EDGE_MOUSE_WRAP)
-	{
-		x = data->win_width - DIST_EDGE_MOUSE_WRAP;
-		mlx_mouse_move(data->mlx, data->win, x, y);
-	}
-}
-
-static int	mouse_motion_handler(int x, int y, t_data *data)
-{
-	static int	old_x = WIN_WIDTH / 2;
-
-	wrap_mouse_position(data, x, y);
-	if (x == old_x)
-		return (0);
-	else if (x < old_x)
-		data->player.has_moved += rotate(data, -1);
-	else if (x > old_x)
-		data->player.has_moved += rotate(data, 1);
-	old_x = x;
-	return (0);
-}
-
 void	listen_for_input(t_data *data)
 {
 	mlx_hook(data->win, ClientMessage, NoEventMask, exit_cub, data);
